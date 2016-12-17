d532a9c8c83401e63afae181a9c530a695ba816f
tjpalanca <mail@tjpalanca.com>
2016-12-18 01:01:42 +0800

    Usability on post scraping - improved and more accurate output - ability to set both time and post number limits

--- src/01-page-scraping.R
+++ src/01-page-scraping.R
@@ -6,17 +6,24 @@
 # for the analysis of fake account behaviors
 
 # Libraries ---------------------------------------------------------------
-library(httr)     # Making API calls
-library(jsonlite) # JSON parsing
+library(httr)      # Making API calls
+library(jsonlite)  # JSON parsing
 
-library(magrittr) # Advanced piping
-library(purrr)    # Functional programming
+library(magrittr)  # Advanced piping
+library(purrr)     # Functional programming
 
-library(stringr)  # String manipulation
-library(dplyr)    # Data frame manipulation
-library(tibble)   # Better data frames
+library(stringr)   # String manipulation
+library(dplyr)     # Data frame manipulation
+library(tibble)    # Better data frames
+library(tidyr)     # Frame shaping
+library(lubridate) # Timestamp manipulation
 
 # Scraping Functions ------------------------------------------------------
+
+convertTimeStamp <- function(ts) {
+  # Converts string timestamp ts into POSIXct format
+  as.POSIXct(strptime(ts, "%Y-%m-%dT%H:%M:%S%z"))
+}
 
 getAppAccessToken <- function(fb_app_id, fb_app_secret) {
   # Get app access token 
@@ -60,11 +67,44 @@ makeFBGraphAPICall <- function(node, query = NULL, version = "v2.8",
   
 }
 
-getFBPagePosts <- function(page_name, n_posts, access_token = fb.tkn) {
+
+getFBPagePosts <- function(page_name, n_posts = Inf, date_limit = -Inf,
+                           access_token = fb.tkn) {
+  # Scrapes posts from a Facebook Page, subject to a limit on either/both
+  # number of posts and or the date (in local system time).
+  #
+  # Args:
+  #   page_name:   the username of the page to be scraped
+  #   n_posts:     limit on the number of posts to be taken
+  #   date_limit:  limit on the minimum date to be scraped
+  #   access_token: access_token (ignore if fb.tkn is already initialized)
+  #
+  # Returns:
+  #   data frame containing information on the posts from the Facebook 
+  #   page that satisfy the limits
   
-  # Initializing call
+  # Raise exception if no limit is specified
+  if (is.infinite(n_posts) & is.infinite(date_limit)) {
+    stop("No post or date limit specified!")
+  }
   
-  # Grab new content
+  # Function: Collection Status
+  showCollectionStatus <- function(dt) {
+    # Shows the collection status given a dataframe with scrape
+    cat(
+      paste0(
+        nrow(dt), 
+        " posts now collected from ",
+        format(
+          convertTimeStamp(min(dt$created_time)), 
+          format = "%Y-%m-%d %H:%M:%S"
+        ), 
+        "\n"
+      )
+    )
+  }
+  
+  # Initialize content
   makeFBGraphAPICall(
     node         = paste0(page_name, "/posts"),
     query        = list(fields = "message,created_time,id,story,attachments"),
@@ -73,33 +113,75 @@ getFBPagePosts <- function(page_name, n_posts, access_token = fb.tkn) {
     content(as = "text") %>% 
     fromJSON(flatten = TRUE) ->
     fbpage.cnt
-  
   cat(paste0("Grabbed ", nrow(fbpage.cnt$data), " posts... "))
   
   posts.dt <- fbpage.cnt$data %>% remove_rownames()
-  
-  cat(paste0(nrow(posts.dt), " posts now collected. \n"))
-  
+  showCollectionStatus(posts.dt)
+    
   # Repeat until n_posts has been reached and next page still exists
-  while (nrow(posts.dt) < n_posts & !is.null(fbpage.cnt$paging$`next`)) {
+  while (nrow(posts.dt) < n_posts & 
+         min(convertTimeStamp(posts.dt$created_time)) > date_limit &
+         !is.null(fbpage.cnt$paging$`next`)) {
     
     # Grab new content
     GET(fbpage.cnt$paging$`next`) %>% 
       content(as = "text") %>% 
       fromJSON(flatten = TRUE) ->
       fbpage.cnt
-    
     cat(paste0("Grabbed ", nrow(fbpage.cnt$data), " posts... "))
     
     # Append posts
-    posts.dt %>% rbind(fbpage.cnt$data %>% remove_rownames()) -> posts.dt
-    
-    cat(paste0(nrow(posts.dt), " posts now collected.\n"))
-    
+    posts.dt %>% bind_rows(fbpage.cnt$data %>% remove_rownames()) -> posts.dt
+    showCollectionStatus(posts.dt)
   }
   
-  return(posts.dt %>% slice(1:n_posts))
+  # Limit to n_posts and date_limit if applicable
+  if (!is.infinite(n_posts)) {
+    posts.dt %<>% slice(1:n_posts) 
+  }
+  # Limit to date limit
+  if (!is.infinite(date_limit)) {
+    posts.dt %<>% 
+      slice(which(convertTimeStamp(posts.dt$created_time) > date_limit))
+  }
   
+  # Flatten out attachments data
+  posts.dt$attachments.data %>% 
+    map_df(
+      function(x) {
+        if (length(x) == 0) {
+          return(data_frame(description = NA))
+        } else {
+          return(x[1,])
+        }
+      }
+    ) -> posts_attachments.dt
+  
+  # Return clean dataset with renamed columns
+  return(
+    cbind(
+      posts.dt %>% select(-attachments.data),
+      posts_attachments.dt
+    ) %>% 
+      mutate(
+        article_url = coalesce(url, target.url),
+        created_time = convertTimeStamp(created_time)
+      ) %>% 
+      select(
+        post_id = id,
+        post_timestamp_utc = created_time,
+        post_message = message,
+        post_story = story,
+        article_id = target.id,
+        article_title = title,
+        article_description = description,
+        article_type = type,
+        article_url,
+        article_media_height = media.image.height,
+        article_media_width = media.image.width,
+        article_media_url = media.image.src
+      ) 
+  )
 }
 
 # Scraping ----------------------------------------------------------------
@@ -114,5 +196,6 @@ fb.tkn <- getAppAccessToken(fb_app_id, fb_app_secret)
 
 getFBPagePosts(
   page_name = "rapplerdotcom",
-  n_posts = 10000
-) -> test
+  date_limit = as.POSIXct("2016-12-17")
+) -> posts.dt
+
