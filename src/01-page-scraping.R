d4beebbe346059d77f6f51bb40fd21d723a924f4
tjpalanca <mail@tjpalanca.com>
2016-12-17 11:14:40 +0800

    Initialize Facebook page scraping

---
+++ src/01-page-scraping.R
@@ -0,0 +1,9 @@
+# Facebook Page Scraping
+# mail@tjpalanca.com
+# 17 Dec 2016
+
+# We scrape the Facebook Graph API in order to collect the data required
+# for the analysis of fake account behaviors
+  
+
+
