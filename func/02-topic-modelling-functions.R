500826f5d1ab401a226120a939285fba1e6b0ffa
Troy James Palanca <tjpalanca@uber.com>
2017-02-23 23:22:01 +0800

    Produce landscape map

--- func/02-topic-modelling-functions.R
+++ func/02-topic-modelling-functions.R
@@ -232,3 +232,31 @@ plotCrossValidatedPerplexity <- function(df) {
       })
     }
 }
+
+computeJensenShannonPCA <- function(wtp) {
+  # Adapted from https://github.com/cpsievert/LDAvis/blob/master/R/createJSON.R
+  # 
+  # Returns coordinates for plotting topics with Jensen Shannon distance as 
+  # the distance measure, projected down to 2 dimensions using principal 
+  # coordinates analysis.
+  #
+  # Args:    wtp:  word-topic probability matrix
+  # Returns: data frame containing the x-y location of each topic 
+  
+  jensenShannon <- function(x, y) {
+    m <- 0.5 * (x + y)
+    0.5 * sum(x * log(x / m)) + 0.5 * sum(y * log(y / m))
+  }
+  
+  wtp %>% 
+    spread(term, beta) %>% 
+    { .[, 2:ncol(.)] } %>%
+    dist(x = ., method = jensenShannon) %>% 
+    cmdscale(k = 2) %>% {
+      data.frame(
+        topic = 1:n_distinct(wtp$topic),
+        x = .[, 1],
+        y = .[, 2]
+      )
+    }
+}
